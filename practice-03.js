404495aeafd8ba995e58d34f2e7d6e4db3b06981
Sasha <aleksandraknott@gmail.com>
2025-01-28 17:40:21 +0000

    add while, do...while, for loops

---
+++ practice-03.js
@@ -0,0 +1,49 @@
+//Loops: while and for
+//while syntax:
+
+// while (condition) {
+//     // code
+//     // so-called "loop body"
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     alert(i);
+//     i++;
+// }
+
+// let i = 3;
+// while (i) {
+//     alert(i);
+//     i--;
+// }
+
+//The "do...while" loop syntax:
+// do {
+//     //loop body
+// } while (condition);
+
+// let i = 0;
+// do {
+//     alert(i)
+//     i++;
+// } while (i < 3);
+
+//The "for" loop syntax:
+// for (begin; condition; step){
+//     // ...loop body ...
+// }
+
+// for (let i = 0; i < 3; i++){
+//     alert(i)
+// }
+
+// run begin
+let i = 0;
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// ...finish, because now i == 3
