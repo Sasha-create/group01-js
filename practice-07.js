21f8a090638df41b9613a9dbe55286b10170c382
Sasha <aleksandraknott@gmail.com>
2025-03-11 18:19:26 +0000

    Iterables, Map and Set

---
+++ practice-07.js
@@ -0,0 +1,113 @@
+//Iterables
+//for..of
+// let range = {
+//     from: 1,
+//     to: 5
+// };
+// //symbol.iterator
+// range[Symbol.iterator] = function () {
+//     return {
+//         current: this.from,
+//         last: this.to,
+
+//         next() {
+//             //it should return the value as an object{done:.., value:...}
+//             if (this.current <= this.last) {
+//                 return { done: false, value: this.current++ };
+//             } else {
+//                 return{done: true}
+//             }
+//         }
+//     }
+// }
+
+// for (let num of range) {
+//     alert(num); //1,2,3,4,5
+// }
+
+// //String is iterable
+// for (let char of "test") {
+//     alert(char); //t, e, s, t
+// }
+
+// let str = 'T6';
+// for (let char of str) {
+//     alert(char); // T, 6
+// }
+
+//Calling an iterator explicitly
+// let str = 'Hello';
+// let iterator = str[Symbol.iterator]();
+
+// while (true) {
+//     let result = iterator.next();
+//     if (result.done) break;
+//     alert(result.value); // H, e, l, l, o
+// }
+
+// //Iterables and array-likes
+// //example the object that is array-like, but not iterable:
+// let arrayLike = {
+//     0: "Hello",
+//     1: "World",
+//     length: 2
+// };
+
+// for(let item of arrayLike){}
+
+//Array.from
+// let arrayLike = {
+//     0: "Hello",
+//     1: "World",
+//     length: 2
+// };
+
+// let arr = Array.from(arrayLike);
+// alert(arr.pop())
+
+
+//Variation1
+// let str = 'T6';
+// let chars = Array.from(str);
+
+// alert(chars[0]); //T
+// alert(chars[1]); //6
+// alert(chars.length); //2
+
+//Variation2
+// let str = 'T6';
+
+// let chars = [];
+// for (let char of str) {
+//     chars.push(char);
+// }
+// alert(chars);
+
+
+//Map and Set
+//Map methods and properties are:
+//  - new Map() - creates the map
+//  - map.set(key, value) - stores the value by the key
+//  - map.get(key) - returns the value by the key. undefined if key does not exist in map
+//  - map.has(key) - returns true if the key exist, false otherwise
+//  - map.delete(key) - removes element by the key
+//  - map.clear() - removes everything from the map
+//  - map.size - returns the current element count
+
+//Iteration over Map
+//  - map.keys() - returns an iterable for keys
+//  - map.values() - returns an iterable for values
+//  - map.entries() - returns an iterable for entries [key, value], it's used by default in for..of
+
+//Set methods:
+//  - new Set([iterable]) - creates the set
+//  - set.add(value) - adds a value, returns the set itself
+//  - set.delete(value) - removes the value
+//  - set.has(value) - returns true if the value exists in the set, otherwise false
+//  - set.clear() - removes evrything from the set
+//  - set.size - is the elements count
+
+//Iteration over Set
+//  - set.keys() - returns an iterable for values
+//  - set.values() - same as set.keys()
+//  - set.entries() - returns an iterable for entries [value, value]
