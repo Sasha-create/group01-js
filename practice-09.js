0623d9a873f8cf974a9666fcc3934d67acf351bc
Sasha <aleksandraknott@gmail.com>
2025-03-25 18:59:25 +0000

    tasks with an array methods

--- practice-09.js
+++ practice-09.js
@@ -57,3 +57,44 @@ function camelize(str) {
 function camelize(str) {
     return str.split('-').map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
 }
+
+//Task2
+let arr = [5, 2, 1, -10, 8]
+arr.sort((a, b) => b - a); //sort in decreasing order
+alert(arr); //8,5,2,1,-10
+
+//Task3
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+let users = [john, pete, mary]
+
+let names = users.map(item => item.name);
+alert(names); //John, Pete, Mary
+
+//Task4
+let numbers = [1, 1, 2, 2, 3, 4, 5, 6, 7, 7];
+function removeDuplicates(array) {
+    return numbers.filter((a,b) => numbers.indexOf(a) === b)//a - current, b - index
+}
+
+//Task5
+let greeting = 'Hello, world!';
+let upperGreeting = greeting.toUpperCase();
+alert(upperGreeting); //'HELLO, WORLD!'
+
+//Task6
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let combined = arr1.concat(arr2);
+console.log(combined); //[1,2,3,4,5,6]
+
+//Task7
+let num = [1, 2, 3, 4, 5];
+let evenNumbers = arr.filter(num => num % 2 === 0);
+console.log(evenNumbers); //[2,4]
+
+//Task8
+let arr3 = [1, 2, 3, 4, 5];
+let found = arr.find(num => num > 3);
+console.log(found); //4
