4e747c6d1a3c71c36433f05907d6be9b15722089
Sasha <aleksandraknott@gmail.com>
2025-05-20 17:40:49 +0100

    javascript tasks with array methods

--- task-3.js
+++ task-3.js
@@ -13,7 +13,24 @@
 
 //palindrome
 //racecar, LOL, mum, mom, level,madam
-function isPalindrome(str) {
-    return str === str.split("").reverse().join("");
-}
-console.log(isPalindrome("racecar")); //true
+// function isPalindrome(str) {
+//     return str === str.split("").reverse().join("");
+// }
+// console.log(isPalindrome("racecar")); //true
+
+//filter out negative numbers
+// let nums = [1, -2, 3, -4];
+// const positives = nums.filter(n => n >= 0);
+// console.log(positives);
+
+//filter out positive numbers
+// let nums = [1, -2, 3, -4];
+// const negative = nums.filter(n => n < 0);
+// console.log(negative);
+
+//check if all elements are even
+let nums = [2, 4, 6];
+const allEven = nums.every(n => n % 2 === 0);
+console.log(allEven); //true
+
+//find first even number
