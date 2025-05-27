afa8a235d4c25e71b32dc98279081ae256af920c
Sasha <aleksandraknott@gmail.com>
2025-05-27 17:27:17 +0100

    javascript tasks with array methods

--- task-3.js
+++ task-3.js
@@ -29,8 +29,24 @@
 // console.log(negative);
 
 //check if all elements are even
-let nums = [2, 4, 6];
-const allEven = nums.every(n => n % 2 === 0);
-console.log(allEven); //true
+// let nums = [2, 4, 6];
+// const allEven = nums.every(n => n % 2 === 0);
+// console.log(allEven); //true
 
 //find first even number
+// let arr = [1, 3, 4, 5, 6, 7, 8];
+// const firstEven = nums.find(n => n % 2 === 0);
+// console.log(firstEven); // 4
+
+//squared number with map
+// let nums = [1, 2, 3];
+// const squares = nums.map(n => n * n); // 1, 4, 9
+
+//check if array includes an element
+// let fruits = [apple, orange, cherry];
+// const hasApple = fruits.includes("apple"); // true
+
+//sort strings alphabetically
+// let fruits = [apple, orange, cherry];
+// const strAlphabet = fruits.sort();
+// console.log(strAlphabet); // [apple, cherry, orange]
