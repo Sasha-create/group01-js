cc9d25d327dce6d8733488f93cae75773de8159e
Sasha <aleksandraknott@gmail.com>
2025-05-27 18:31:49 +0100

    browser object model

--- practice-10.js
+++ practice-10.js
@@ -1,9 +1,42 @@
 //DOM tree (Document Object Model)
 //BOM (Browser object Model)
 //Javascript
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
+// const ul = document.querySelector('ul');
+// const button = document.querySelector('button');
 
-const li = document.createElement('li');
-li.textContent = 'Item2';
-ul.appendChild(li);
+// const li = document.createElement('li');
+// li.textContent = 'Item2';
+// ul.appendChild(li);
+
+//BOM
+//Navigator
+// Browser
+
+// console.log(navigator.userAgent);
+
+//Platform
+// console.log(navigator.platform);
+
+//Location
+//Get URL
+
+// console.log(location.href);
+// console.log(location.port);
+
+//History
+// history.back();
+// history.forward();
+
+//BOM
+//Alert
+// alert("hello!");
+
+
+//Confirm
+// const confirmResult = confirm("Do you want to learn javascipt?");
+// console.log(confirmResult);
+
+
+//Prompt
+// const promptResult = prompt("Who you are in the real life?");
+// console.log(promptResult);
